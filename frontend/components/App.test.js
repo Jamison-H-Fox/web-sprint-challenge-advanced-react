a897128c56467c4643ec7d4e813bf27b26e24832
Jamison Fox <fox.jamison@outlook.com>
2022-12-17 14:20:32 -0700

    Tests written

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,15 @@
+import AppFunctional from "./AppFunctional"
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import * as rtl from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
 // Write your tests here
 test('sanity', () => {
   expect(true).toBe(true)
 })
+
+test('Component renders correctly', () => {
+  render (<AppFunctional />)
+})
+
