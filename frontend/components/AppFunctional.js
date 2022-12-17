c8ad35b676982dcc4731ae865ad94f063530b87f
Jamison Fox <fox.jamison@outlook.com>
2022-12-17 11:05:58 -0700

    Got reset btn, stepCounter, and coordinate finder working in functional component

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Suggested initial states
 const initialMessage = ''
@@ -7,33 +7,64 @@ const initialSteps = 0
 const initialIndex = 4 // the index the "B" is at
 
 export default function AppFunctional(props) {
+  // State declarations
+  const [index, setIndex] = useState(initialIndex);
+  const [stepCounter, setStepCounter] = useState(initialSteps);
+
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
 
-  function getXY() {
+  function getXY(index) {
     // It it not necessary to have a state to track the coordinates.
-    // It's enough to know what index the "B" is at, to be able to calculate them.
+    // It's enough to know what index the "B" is at, to be able to calculate them.    
+    const coords = [0, 0];
+
+    if (index < 3) {
+      coords[1] = 1;
+    } else if (index < 6) {
+      coords[1] = 2;
+    } else {
+      coords[1] = 3;
+    }
+
+    if (index === 0 || index === 3 || index === 6) {
+      coords[0] = 1;
+    } else if (index === 1 || index === 4 || index === 7) {
+      coords[0] = 2;
+    } else {
+      coords[0] = 3;
+    }
+
+    return coords
   }
 
-  function getXYMessage() {
+  function getXYMessage(cb) {
     // It it not necessary to have a state to track the "Coordinates (2, 2)" message for the user.
     // You can use the `getXY` helper above to obtain the coordinates, and then `getXYMessage`
     // returns the fully constructed string.
+    const x = cb[0];
+    const y = cb[1];
+
+    const msgString = `Coordinates (${x}, ${y})`;
+    return msgString;
   }
 
   function reset() {
-    // Use this helper to reset all states to their initial values.
+    setIndex(initialIndex);
+    setStepCounter(initialSteps);
   }
 
   function getNextIndex(direction) {
     // This helper takes a direction ("left", "up", etc) and calculates what the next index
     // of the "B" would be. If the move is impossible because we are at the edge of the grid,
     // this helper should return the current index unchanged.
+    
   }
 
   function move(evt) {
     // This event handler can use the helper above to obtain a new index for the "B",
     // and change any states accordingly.
+    setStepCounter(stepCounter + 1);
   }
 
   function onChange(evt) {
@@ -47,8 +78,8 @@ export default function AppFunctional(props) {
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">{getXYMessage(getXY(index))}</h3>
+        <h3 id="steps">You moved {stepCounter} time{stepCounter == 1 ? '' : 's'}</h3>
       </div>
       <div id="grid">
         {
@@ -60,14 +91,14 @@ export default function AppFunctional(props) {
         }
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">Message will go here</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button onClick={(evt) => move(evt)} id="left">LEFT</button>
+        <button onClick={(evt) => move(evt)} id="up">UP</button>
+        <button onClick={(evt) => move(evt)} id="right">RIGHT</button>
+        <button onClick={(evt) => move(evt)} id="down">DOWN</button>
+        <button onClick={() => reset()} id="reset">reset</button>
       </div>
       <form>
         <input id="email" type="email" placeholder="type email"></input>
---
+++ toDo.md
@@ -0,0 +1,10 @@
+[x] set coordinates h3 to track actual coordinates
+    [x] Create state for index/position
+    [x] getXY will take index and determine (x,y)
+[x] Increment stepCounter with each move, reset with reset btn
+    [x] Create state for stepCounter
+    [x] move handler should increment stepCounter
+    [x] reset handler should set stepCounter to 0
+[ ] Update index when a direction is clicked
+    [ ] figure out how to know which direction was clicked 🤔
+    [ ] 
